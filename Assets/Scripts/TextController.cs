11724fb56ea264640abff2ab9c59155a3a35f9c1
h2uh2u <gmldnjs526@naver.com>
2020-12-17 21:24:21 +0900

    Implement print in TextController

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TextController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI messageText;
+    public Scrollbar textScrollbar;
 
-    // Update is called once per frame
-    void Update()
+    public void print(string text)
     {
-        
+        List<string> messageTextList = messageText.text.Split('\n').ToList<string>();
+        messageTextList.Add(text);
+        if (messageTextList.Count > 50)
+        {
+            messageTextList.RemoveAt(0);
+        }
+        messageText.text = string.Join("\n", messageTextList.ToArray());
+        textScrollbar.value = 0;
     }
 }
